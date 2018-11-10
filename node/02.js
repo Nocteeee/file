4b688c9337e51a1b02580d89d7c71d2ad18b314d
liunian <965205737@qq.com>
2018-11-10 16:13:22 +0800

    删除多余文件

--- node/02.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-fs.mkdir('duff',function(){
-    fs.readFile('text.txt','utf8',function(err,data){
-        fs.writeFile('./duff/writer.txt',data,function(){
-            console.log('copy is successfully')
-        })
-    })
-})
